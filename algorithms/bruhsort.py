70db55daed6f02bc1436eced2f77792dc99d4d43
Wiktor <falekwiktor@gmail.com>
2023-11-08 10:41:13 +0100

    Delete algorithms/bruhsort.py

--- algorithms/bruhsort.py
+++
@@ -1,7 +0,0 @@
-def bruhsort(array):
-    arr = list(array)
-    target_arr = []
-    while arr:
-        target_arr.append(arr.pop(arr.index(min(arr))))
-        yield arr + target_arr
-    return target_arr
